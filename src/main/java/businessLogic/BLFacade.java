b0f70e67c2f78be460a13a613703d6744fa3aa20
MIKEL BERLANGA SAN JOSE <mberlanga002@ikasle.ehu.eus>
2026-02-28 17:48:56 +0100

    uml

--- src/main/java/businessLogic/BLFacade.java
+++ src/main/java/businessLogic/BLFacade.java
@@ -72,7 +72,7 @@ public interface BLFacade  {
 	
 	@WebMethod public User isLogin(String email);
 	
-	@WebMethod public void buyProduct(Sale sale,String email);
+	@WebMethod public void buyProduct(Sale sale, String email);
 	
 	@WebMethod 	public User doesAccountNumber(String zenb);
 	
